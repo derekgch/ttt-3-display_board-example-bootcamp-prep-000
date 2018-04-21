d4f294082b7bf5a2abb0abd1e334a0f1ca9ff118
derek chen <derek.g.chen@gmail.com>
2018-04-21 20:26:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
-  board = ""
+  board = []
 
   j=0;
 
-  line = "   |   |   "
+  line = "   |   |   \n"
   sp = "-----------"
 
   while j < 3
